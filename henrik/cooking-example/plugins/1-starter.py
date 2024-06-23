3b9ce170170dc5e87719e9d1afa989354f687dbc
Henrik <henrrb@stud.ntnu.no>
2024-06-23 23:59:12 +0200

    Add first code test

--- henrik/cooking-example/plugins/1-starter.py
+++ henrik/cooking-example/plugins/1-starter.py
@@ -1,6 +1,98 @@
 import semantic_kernel as sk
-kernel = sk.Kernel()
-
+import os
+import asyncio
+from dotenv import load_dotenv
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
-api_key, org_id = sk.openai_settings_from_dot_env()
-kernel.add_chat_service("chat-gpt", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
+from semantic_kernel.functions import KernelArguments
+load_dotenv()
+
+# Setup
+kernel = sk.Kernel()
+api_key = os.getenv("OPENAI_API_KEY")
+prompt = "Chicken Adobo Filipino Style"
+plugins_directory = "../plugins"
+
+# OpenAI Service
+kernel.add_service(OpenAIChatCompletion(
+    ai_model_id="gpt-3.5-turbo",
+    service_id="default",
+    api_key=api_key
+    ))
+
+# Cooking Plugin
+cooking_skill = kernel.add_plugin(
+    parent_directory=plugins_directory,
+    plugin_name="Cooking"
+    )
+recipe_function = cooking_skill["RecipeGenerator"]
+
+# Marketing Plugin
+advertisement_skill = kernel.add_plugin(
+    parent_directory=plugins_directory,
+    plugin_name="Marketing"
+    )
+advertisment_function = advertisement_skill["AdvertisementGenerator"]
+
+
+## With async
+async def get_recipe(prompt):
+    
+    recipe = await kernel.invoke(
+        recipe_function,
+        KernelArguments(
+            input=prompt,
+            format=""
+            )
+    )
+    return recipe
+
+async def get_advertisement(recipe):
+    advertisement = await kernel.invoke(
+        advertisment_function,
+        KernelArguments(
+            input=recipe
+            )
+    )
+    return advertisement
+
+
+### With main function
+async def main():
+    
+    # load_dotenv()
+    recipe = await get_recipe(prompt)
+    advertisement = await get_advertisement(recipe)
+
+    print("Recipe: ", recipe)
+    print()
+    print("Advertisement: ", advertisement)
+
+asyncio.run(main())
+
+### Without main function
+# recipe = asyncio.run(get_recipe(prompt))
+# advertisement = asyncio.run(get_advertisement(recipe))
+
+# print("Recipe: ", recipe)
+# print()
+# print("Advertisement: ", advertisement)
+
+
+
+## Without async
+
+# Results
+# recipe_result = recipe_function(prompt)
+# advertisement_result = advertisment_function(recipe_result)
+# print("Recipe: ", recipe_result)
+# print("Advertisement: ", advertisement_result)
+
+# recipe = await kernel.invoke(
+#     recipe_function,
+#     KernelArguments(
+#         input=prompt,
+#         format=""
+#         )    
+# )
+# recipe = await kernel.invoke(recipe_function, input=prompt, format="")     # Alternative
+# print(recipe)
